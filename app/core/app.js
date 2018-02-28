c089c7e14fae6f52e2e32ed84ad107be53f30daf
Tharyck Souto Vasconcelos <tharycksouto@gmail.com>
2018-02-28 00:07:02 -0300

    inicio

---
+++ app/core/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('funcional', ['ngRoute', 'myApp.version']).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: './view/submit'});
+}]);
